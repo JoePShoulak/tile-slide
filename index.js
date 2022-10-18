1600646bed8f3aa4713615bb2d1ba96e7b11875c
Joe P Shoulak <joe@squarecompass.io>
2022-10-18 18:45:50 -0500

    full functionality

--- index.js
+++ index.js
@@ -6,22 +6,42 @@ const grid = new Grid();
 grid.randomEmptyCell().tile = new Tile(grid);
 grid.randomEmptyCell().tile = new Tile(grid);
 
+function canMove(cells) {
+    return cells.some(group => {
+      return group.some((cell, index) => {
+        if (index === 0) return false
+        if (cell.tile == null) return false
+        const moveToCell = group[index - 1]
+        return moveToCell.canAccept(cell.tile)
+      })
+    })
+  }
+
+const canMoveUp = () => canMove(grid.cellsByColumn)
+const canMoveDown = () => canMove(grid.cellsByColumn.map(column => [...column].reverse()))
+const canMoveLeft = () => canMove(grid.cellsByRow)
+const canMoveRight = () => canMove(grid.cellsByRow.map(column => [...column].reverse()))
+
 const handleInput = (event) => {
     switch (event.key) {
         case "ArrowUp":
         case "w":
+            if (!canMoveUp()) return setupInput()
             moveUp();
             break;
         case "ArrowDown":
         case "s":
+            if (!canMoveDown()) return setupInput()
             moveDown();
             break;
         case "ArrowLeft":
         case "a":
+            if (!canMoveLeft()) return setupInput()
             moveLeft();
             break;
         case "ArrowRight":
         case "d":
+            if (!canMoveRight()) return setupInput()
             moveRight();
             break;
         default:
@@ -31,8 +51,13 @@ const handleInput = (event) => {
 
     grid.cells.forEach(cell => cell.mergeTiles())
 
-    grid.randomEmptyCell().tile = new Tile(grid);
+    const newTile = new Tile(grid)
+    grid.randomEmptyCell().tile = newTile;
 
+    if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+        return newTile.waitForTransition(true).then(() => alert("You lose."))   
+    }
+    
     setupInput();
 }
 
